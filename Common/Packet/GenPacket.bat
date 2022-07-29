3ffe747e19e11cd91243b6c9465866901d239194
charg2dang <charg2dang@kakao.com>
2022-07-29 20:07:15 +0900

    chore: git ingone unity 환경에 맞춰 추가

--- Common/Packet/GenPacket.bat
+++ Common/Packet/GenPacket.bat
@@ -5,8 +5,11 @@ START ../../PacketGenerator/bin/PacketGenerator.exe ../../PacketGenerator/PDL.xm
 
 rem XCOPY [ /Y 같은 이름 파일 덮어 쓰기 ]  [ "경로" ]
 XCOPY /Y GenPackets.cs "../../DummyClient/Packet"
+XCOPY /Y GenPackets.cs "../../Client/Assets/Scripts/Packet"
 XCOPY /Y GenPackets.cs "../../Server/Packet"
+
 XCOPY /Y ClientPacketManager.cs "../../DummyClient/Packet"
+XCOPY /Y ClientPacketManager.cs "../../Client/Assets/Scripts/Packet"
 XCOPY /Y ServerPacketManager.cs "../../Server/Packet"
 
 
